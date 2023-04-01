f1bb538dc8798d339162a0650b7261bdfad70dbb
Mr00Magician <meanasnadeem@gmail.com>
2023-04-01 21:35:14 +0530

    changed location of app.py

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder='./client/templates', static_folder='./client/static')
+app = Flask(__name__, template_folder='../client/templates', static_folder='../client/static')
 
 @app.route('/')
 def home():
