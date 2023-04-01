1c8363b53e5b5252fa12bc9792c2b1e551ef456a
Mr00Magician <meanasnadeem@gmail.com>
2023-04-01 17:34:21 +0530

    added initial server and client-side files

---
+++ server/app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='../client/templates', static_folder='../client/static')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
