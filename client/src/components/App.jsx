16b92b15472ce8a588833296e6fcbcb6321c8f1b
Mr00Magician <meanasnadeem@gmail.com>
2023-05-07 23:15:51 +0530

    removed code added for initial testing of server

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,30 +1,15 @@
 import React from "react";
-import { useState, useEffect } from "react";
 import NavBar from "./NavBar";
 import Canvas from "./Canvas";
 import "./App.css"
 
 function App() {
-    const [data, setData] = useState([{}])
-
-    useEffect(() => {
-        fetch("./data").then(
-            res => res.json()
-        ).then(
-            data => {
-                setData(data);
-                console.log(data);
-            }
-        ).catch((error) => console.warn('Something went wrong', error))
-    }, [])
-
     return (
         <div>
             <header>
                 <NavBar />
             </header>
             <Canvas />
-            <h1>{data.name}</h1>
         </div>
     )
 }
