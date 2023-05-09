8211f5b81922ba9f6b2f7fbbb04addf9a0dc6a96
Mr00Magician <meanasnadeem@gmail.com>
2023-05-10 00:16:48 +0530

    Set up server to receive canvas image & other fixes
    Canvas.jsx
    - Fixed a bug due to which the prediction was not showing up in the
    `textarea`
    - This was done by making the `getPrediction` function asynchronous and
    awaiting for the fetch response before showing it on the `textarea`
    
    app.py
    - The server has now been set up to receive the image drawn on canvas
    and use a trained neural network to make a prediction and send a
    response to the client side.
    
    1 new file added
    - A new file named `CNN_extended_dataset.h5` has been added. This is the
    file that contains the trained neural network used by the server.
    
    Navbar.jsx
    - the `other projects` anchor tag is now linked to my portfolio website

--- server/app.py
+++ server/app.py
@@ -1,10 +1,29 @@
 from flask import Flask, render_template, request, jsonify
+import numpy as np
+import tensorflow as tf
+import cv2
+import matplotlib.pyplot as plt
 
 app = Flask(__name__, template_folder='../client/build', static_folder='../client/build/static')
+
+loaded_CNN = tf.keras.models.load_model('CNN_extended_dataset.h5') 
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+@app.route('/get-prediction', methods = ['POST'])
+def get_prediction():
+    img_array = request.json.get('data')
+    print(img_array)
+    img_array = np.array(img_array, dtype=np.uint8).reshape(400, 400)
+    img_array = cv2.resize(img_array, (28, 28))
+    plt.imsave('imgData.png', img_array, cmap = 'gray')
+    img_array = img_array.reshape(1, 28, 28)
+
+    pred = loaded_CNN.predict([img_array])
+    final_pred = np.argmax(pred)
+    return str(final_pred)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
