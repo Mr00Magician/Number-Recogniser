8df27594ca0330f918c596dd3f059d8825934a87
Mr00Magician <meanasnadeem@gmail.com>
2023-06-20 12:25:32 +0530

    UI: Makes `NavBar` element responsive for portrait mode
    NavBar.css
    - adds an `@media` rule to change the size of `NavBar` element
    when orientation changes
    - adds an `@media` rule to hide the navbar anchor links and show a
    hamburger menu instead
    
    NavBar.jsx
    - adds a `useLayoutEffect` react hook to detect touch on the
    hamburger menu and change its styling
    - adds a div for the hamburger menu
    
    build/
    - Updates build files with the latest src files in this commit

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import "./NavBar.css"
 
 function NavBar() {
+
+    useLayoutEffect(() => {
+        let menuOpen = false;
+        let hamburgerMenu = document.querySelector('.menu');
+        let hamburger = document.querySelector('.hamburger');
+        hamburgerMenu.addEventListener('touchstart', e => {
+            if (menuOpen) {
+                hamburger.classList.remove('open');
+                menuOpen = false;
+            }
+            else {
+                hamburger.classList.add('open');
+                menuOpen = true;
+            }
+        })
+    })
+
     return (
         <nav>
             <h1>Number Recogniser</h1>
@@ -9,6 +26,9 @@ function NavBar() {
                 <li><a href="https://github.com/Mr00Magician/Number-Recogniser">Github Repo</a></li>
                 <li><a href="https://me-anas-nadeem.carrd.co">Other Projects</a></li>
             </ul>
+            <div className="menu">
+                <div className="hamburger"></div>
+            </div>
         </nav>
     )
 }
